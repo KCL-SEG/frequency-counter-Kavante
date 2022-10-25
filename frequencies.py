1b41c70c3df19670aefc97c60fbe89fc5f72e609
Kavante <77113850+Kavante@users.noreply.github.com>
2022-10-25 10:56:43 +0100

    first commit

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    for current in items:
+        #checks if current is not a string 
+        check = type(current) == str
+        if check == False:
+            current = str(current)
+        frequencies[current] = frequencies.get(current,0) + 1
+
     return frequencies
